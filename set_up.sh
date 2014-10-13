9e0d592f8b578a27877efa5f3ed7b420af68ba80
muchweb <aleks@s-ko.net>
2014-10-13 22:06:52 +0100

    Setup script

---
+++ set_up.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo ' - setting keyboard to GB'
+./scripts/set_keyboard_gb.sh
+
+# Changing background colour
+# Awesome wallpaper command
+# don't work for some reason...
+xsetroot -solid '#2D2D2D'
