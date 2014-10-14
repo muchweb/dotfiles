037e170f66899df7ba7b5d453b133e700e5c2c3b
muchweb <aleks@s-ko.net>
2014-10-14 17:47:14 +0100

    setup script update

--- set_up.sh
+++ set_up.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo ' - setting keyboard to GB'
+
+# Set keyboard to GB
 setxkbmap -layout gb
 
 # Changing background colour
 # Awesome wallpaper command
 # don't work for some reason...
 xsetroot -solid '#2D2D2D'
+
+# Screen locker
+xscreensaver -no-splash &
